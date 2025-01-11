d562a5a04f28d8a7efacfed0490e114d8f54cdac
sithid <demonicurges05@gmail.com>
2025-01-11 16:26:03 -0500

    misc

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,9 +1,3 @@
-/*
- * Linked List:  I will actually be moving this code to its own little project.
- * Option<..> can be used for Some(value) or None (like in the event the node does not have a next because it is the last node in the list).
- * Box<..> is a smart pointer for data allocation on the heap.  Because Node is recursive ( reference to a Node struct: head)
- */
-
 #[derive(Debug)]
 struct Person {
     name: String,
