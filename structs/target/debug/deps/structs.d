da77a98249e006195404d45f64e4cdf77af94a2e
sithid <demonicurges05@gmail.com>
2025-01-11 02:52:54 -0500

    structs: added

---
+++ structs/target/debug/deps/structs.d
@@ -0,0 +1,5 @@
+C:\Users\demon\OneDrive\Documents\GitHub\rust-structures\structs\target\debug\deps\structs.exe: src/main.rs
+
+C:\Users\demon\OneDrive\Documents\GitHub\rust-structures\structs\target\debug\deps\structs.d: src/main.rs
+
+src/main.rs:
---
+++ structs/target/debug/structs.d
@@ -0,0 +1,1 @@
+C:\Users\demon\OneDrive\Documents\GitHub\rust-structures\structs\target\debug\structs.exe: C:\Users\demon\OneDrive\Documents\GitHub\rust-structures\structs\src\main.rs
